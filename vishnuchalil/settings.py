c9472fa05b8d3c2f3e79f3affd6ce4c54436327a
vishnu-chalil <vishnu080797@gmail.com>
2019-08-10 16:10:35 +0530

    first commit

--- vishnuchalil/settings.py
+++ vishnuchalil/settings.py
@@ -14,13 +14,13 @@ import os
 import dj_database_url
 import dotenv
 
-dotenv_file = os.path.join(BASE_DIR, ".env")
-if os.path.isfile(dotenv_file):
-    dotenv.load_dotenv(dotenv_file)
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+dotenv_file = os.path.join(BASE_DIR, ".env")
+if os.path.isfile(dotenv_file):
+    dotenv.load_dotenv(dotenv_file)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
